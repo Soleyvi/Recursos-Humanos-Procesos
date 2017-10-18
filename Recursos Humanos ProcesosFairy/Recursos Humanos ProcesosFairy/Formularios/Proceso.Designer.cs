fcb00a22f34ffeeab397e40095c03c855af62da5
Soleyvi Martinez <solemartinez_29@hotmail.com>
2017-10-17 22:41:11 -0600

    fondo

--- Recursos Humanos ProcesosFairy/Recursos Humanos ProcesosFairy/Formularios/Proceso.Designer.cs
+++ Recursos Humanos ProcesosFairy/Recursos Humanos ProcesosFairy/Formularios/Proceso.Designer.cs
@@ -28,10 +28,10 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.btRECLUTAMIENTO = new System.Windows.Forms.Button();
             this.btnProceso = new System.Windows.Forms.Button();
             this.btnContratacion = new System.Windows.Forms.Button();
-            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
             this.btRegresar = new System.Windows.Forms.Button();
             this.btnSalir = new System.Windows.Forms.Button();
             this.SELECCIONE = new System.Windows.Forms.GroupBox();
@@ -67,17 +67,9 @@
             this.btnContratacion.Text = "CONTRATACION";
             this.btnContratacion.UseVisualStyleBackColor = true;
             // 
-            // menuStrip1
-            // 
-            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
-            this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(671, 24);
-            this.menuStrip1.TabIndex = 5;
-            this.menuStrip1.Text = "menuStrip1";
-            // 
             // btRegresar
             // 
-            this.btRegresar.Location = new System.Drawing.Point(483, 233);
+            this.btRegresar.Location = new System.Drawing.Point(47, 309);
             this.btRegresar.Name = "btRegresar";
             this.btRegresar.Size = new System.Drawing.Size(75, 23);
             this.btRegresar.TabIndex = 6;
@@ -86,7 +78,7 @@
             // 
             // btnSalir
             // 
-            this.btnSalir.Location = new System.Drawing.Point(584, 233);
+            this.btnSalir.Location = new System.Drawing.Point(156, 309);
             this.btnSalir.Name = "btnSalir";
             this.btnSalir.Size = new System.Drawing.Size(75, 23);
             this.btnSalir.TabIndex = 7;
@@ -97,9 +89,9 @@
             // 
             this.SELECCIONE.Controls.Add(this.radioButton2);
             this.SELECCIONE.Controls.Add(this.radioButton1);
-            this.SELECCIONE.Location = new System.Drawing.Point(228, 28);
+            this.SELECCIONE.Location = new System.Drawing.Point(190, 37);
             this.SELECCIONE.Name = "SELECCIONE";
-            this.SELECCIONE.Size = new System.Drawing.Size(200, 108);
+            this.SELECCIONE.Size = new System.Drawing.Size(385, 178);
             this.SELECCIONE.TabIndex = 8;
             this.SELECCIONE.TabStop = false;
             this.SELECCIONE.Text = "Seleccione";
@@ -130,21 +122,22 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(671, 268);
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(644, 373);
             this.Controls.Add(this.SELECCIONE);
             this.Controls.Add(this.btnSalir);
             this.Controls.Add(this.btRegresar);
             this.Controls.Add(this.btnContratacion);
             this.Controls.Add(this.btnProceso);
             this.Controls.Add(this.btRECLUTAMIENTO);
-            this.Controls.Add(this.menuStrip1);
-            this.MainMenuStrip = this.menuStrip1;
+            this.DoubleBuffered = true;
+            this.ForeColor = System.Drawing.Color.Black;
             this.Name = "Form1";
             this.Text = "Form1";
             this.SELECCIONE.ResumeLayout(false);
             this.SELECCIONE.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
@@ -153,7 +146,6 @@
         private System.Windows.Forms.Button btRECLUTAMIENTO;
         private System.Windows.Forms.Button btnProceso;
         private System.Windows.Forms.Button btnContratacion;
-        private System.Windows.Forms.MenuStrip menuStrip1;
         private System.Windows.Forms.Button btRegresar;
         private System.Windows.Forms.Button btnSalir;
         private System.Windows.Forms.GroupBox SELECCIONE;
